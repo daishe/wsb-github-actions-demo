c3adc8a410b9c072edf1199b7ef075897874c33b
Marek Dalewski <marek@dalewski.pl>
2022-06-17 12:29:51 +0200

    Emphasize greeting

--- src/hello.cpp
+++ src/hello.cpp
@@ -1,5 +1,5 @@
 #include "hello.hpp"
 
 std::string hello(std::string_view name) {
-  return std::string("Hello, ").append(name).append("!");
+  return std::string("Hello, ").append(name).append("!!!");
 }
